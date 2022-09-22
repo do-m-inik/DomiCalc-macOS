dfc663f7f85c2e22818ffed090963b826f18b135
Dominik Schäfer <mail@do-m-inik.at>
2022-09-22 22:55:00 +0200

    Begin of the calculator app for macOS

--- DomiCalc macOS/ContentView.swift
+++ DomiCalc macOS/ContentView.swift
@@ -8,13 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var textInput: String = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("0")
+            TextField("Text Input", text: $textInput)
+            HStack {
+                Button("7", action: typeSeven)
+                Button("8", action: typeEight)
+                Button("9", action: typeNine)
+                Button("/", action: typeDiv)
+            }
+            HStack {
+                Button("4", action: typeFour)
+                Button("5", action: typeFive)
+                Button("6", action: typeSix)
+                Button("*", action: typeMul)
+            }
+            HStack {
+                Button("1", action: typeOne)
+                Button("2", action: typeTwo)
+                Button("3", action: typeThree)
+                Button("-", action: typeMinus)
+            }
+            HStack {
+                Button("0", action: typeZero)
+                Button(".", action: typeComma)
+                Button("=", action: typeSolve)
+                Button("+", action: typePlus)
+            }
         }
+        
         .padding()
     }
 }
---
+++ DomiCalc macOS/Input.swift
@@ -0,0 +1,74 @@
+//
+//  ButtonInput.swift
+//  DomiCalc macOS
+//
+//  Created by Dominik Schäfer on 22.09.22.
+//
+
+import Foundation
+
+var textInput = ""
+
+func typeOne() {
+    
+}
+
+func typeTwo() {
+    
+}
+
+func typeThree() {
+    
+}
+
+func typeFour() {
+    
+}
+
+func typeFive() {
+    
+}
+
+func typeSix() {
+    
+}
+
+func typeSeven() {
+    
+}
+
+func typeEight() {
+    
+}
+
+func typeNine() {
+    
+}
+
+func typeZero() {
+    
+}
+
+func typeDiv() {
+    
+}
+
+func typeMul() {
+    
+}
+
+func typeMinus() {
+    
+}
+
+func typePlus() {
+    
+}
+
+func typeSolve() {
+    
+}
+
+func typeComma() {
+    
+}
