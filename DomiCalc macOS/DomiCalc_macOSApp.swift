6ffbbf15650904301919c08973ee9cba4b1c6a1d
Dominik Schäfer <mail@do-m-inik.at>
2022-09-22 22:09:45 +0200

    Initial Commit

---
+++ DomiCalc macOS/DomiCalc_macOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  DomiCalc_macOSApp.swift
+//  DomiCalc macOS
+//
+//  Created by Dominik Schäfer on 22.09.22.
+//
+
+import SwiftUI
+
+@main
+struct DomiCalc_macOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DomiCalc macOSUITests/DomiCalc_macOSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  DomiCalc_macOSUITestsLaunchTests.swift
+//  DomiCalc macOSUITests
+//
+//  Created by Dominik Schäfer on 22.09.22.
+//
+
+import XCTest
+
+final class DomiCalc_macOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
