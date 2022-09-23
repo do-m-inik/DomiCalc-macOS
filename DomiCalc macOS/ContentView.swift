57ab859daba3d23018401ab7d079a1ddd3fe4784
Dominik Schäfer <mail@do-m-inik.at>
2022-09-23 20:07:57 +0200

    Added 4 more buttons and made buttons larger

--- DomiCalc macOS/ContentView.swift
+++ DomiCalc macOS/ContentView.swift
@@ -9,34 +9,99 @@ import SwiftUI
 
 struct ContentView: View {
     @State var textInput: String = ""
-    
     var body: some View {
         VStack {
             Text("0")
             TextField("Text Input", text: $textInput)
             HStack {
-                Button("7", action: typeSeven)
-                Button("8", action: typeEight)
-                Button("9", action: typeNine)
-                Button("/", action: typeDiv)
+                Button(action: typeSeven) {
+                    Text("C")
+                        .frame(maxWidth: 100)
+                }
+                Button(action: typeEight) {
+                    Text("^")
+                        .frame(maxWidth: 100)
+                }
+                Button(action: typeNine) {
+                    Text("(")
+                        .frame(maxWidth: 100)
+                }
+                Button(action: typeDiv) {
+                    Text(")")
+                        .frame(maxWidth: 100)
+                }
             }
             HStack {
-                Button("4", action: typeFour)
-                Button("5", action: typeFive)
-                Button("6", action: typeSix)
-                Button("*", action: typeMul)
+                Button(action: typeSeven) {
+                    Text("7")
+                        .frame(maxWidth: 100)
+                }
+                Button(action: typeEight) {
+                    Text("8")
+                        .frame(maxWidth: 100)
+                }
+                Button(action: typeNine) {
+                    Text("9")
+                        .frame(maxWidth: 100)
+                }
+                Button(action: typeDiv) {
+                    Text("/")
+                        .frame(maxWidth: 100)
+                }
             }
             HStack {
-                Button("1", action: typeOne)
-                Button("2", action: typeTwo)
-                Button("3", action: typeThree)
-                Button("-", action: typeMinus)
+                Button(action: typeFour) {
+                    Text("4")
+                        .frame(maxWidth: 100)
+                }
+                Button(action: typeFive) {
+                    Text("5")
+                        .frame(maxWidth: 100)
+                }
+                Button(action: typeSix) {
+                    Text("6")
+                        .frame(maxWidth: 100)
+                }
+                Button(action: typeMul) {
+                    Text("*")
+                        .frame(maxWidth: 100)
+                }
             }
             HStack {
-                Button("0", action: typeZero)
-                Button(".", action: typeComma)
-                Button("=", action: typeSolve)
-                Button("+", action: typePlus)
+                Button(action: typeOne) {
+                    Text("1")
+                        .frame(maxWidth: 100)
+                }
+                Button(action: typeTwo) {
+                    Text("2")
+                        .frame(maxWidth: 100)
+                }
+                Button(action: typeThree) {
+                    Text("3")
+                        .frame(maxWidth: 100)
+                }
+                Button(action: typeMinus) {
+                    Text("-")
+                        .frame(maxWidth: 100)
+                }
+            }
+            HStack {
+                Button(action: typeZero) {
+                    Text("0")
+                        .frame(maxWidth: 100)
+                }
+                Button(action: typeComma) {
+                    Text(".")
+                        .frame(maxWidth: 100)
+                }
+                Button(action: typeSolve) {
+                    Text("=")
+                        .frame(maxWidth: 100)
+                }
+                Button(action: typePlus) {
+                    Text("+")
+                        .frame(maxWidth: 100)
+                }
             }
         }
         
